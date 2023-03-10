f943339fee8971ff01dec5831a7f1f50c5772511
Omar Rendon <omarcr.96@gmail.com>
2023-03-10 01:17:27 -0600

    Install react-native-navigator with its respective dependecies and install react-native-vector-icons with its respective configuration

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Icon name="star-outline" size={40} />
+    </View>
+  );
+};
