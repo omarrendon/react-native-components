b74a767f804a18f5ebbbcf58e744186449be813e
Omar Rendon <omarcr.96@gmail.com>
2023-03-17 01:03:07 -0600

    Create HeaderComponet

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,45 +1,22 @@
-import {Text, View, StyleSheet, FlatList} from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {View, StyleSheet, FlatList} from 'react-native';
 import {FlatListMenuItem} from '../components/FlatListMenuItem';
-import {MenuItem} from '../interfaces/appInterfaces';
+import {HeaderTitle} from '../components/HeaderTitle';
+import {menuItems} from '../menuItems';
 import {themeStyles} from '../theme/appTheme';
 
-const menuItems: MenuItem[] = [
-  {
-    name: 'Animation 101',
-    icon: 'cube-outline',
-    component: 'Animation101',
-  },
-  {
-    name: 'Animatioin 102',
-    icon: 'albums-outline',
-    component: 'Animation102',
-  },
-];
-
 export const Home = () => {
-  const {top} = useSafeAreaInsets();
-
-  const RenderListHeader = () => {
-    return (
-      <View style={{marginTop: top + 20, marginBottom: 20}}>
-        <Text style={themeStyles.title}>Opciones del Menú</Text>
-      </View>
-    );
-  };
-
-  const ItemSeparator = () => {
+  const itemSeparator = () => {
     return <View style={styles.itemSeparator} />;
   };
 
   return (
     <View style={{...themeStyles.globalMargin, ...styles.container}}>
-      <RenderListHeader />
-      {/* <ItemSeparator /> */}
       <FlatList
         data={menuItems}
         renderItem={({item}) => <FlatListMenuItem menuItem={item} />}
         keyExtractor={item => item.name}
+        ListHeaderComponent={() => <HeaderTitle title="Opciones del Menú" />}
+        ItemSeparatorComponent={itemSeparator}
       />
     </View>
   );
@@ -52,8 +29,6 @@ const styles = StyleSheet.create({
   itemSeparator: {
     borderBottomWidth: 1,
     opacity: 0.4,
-    width: 1,
-    height: 1,
     marginVertical: 8,
   },
 });
