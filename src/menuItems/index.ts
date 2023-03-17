a67505df259db5feb812c11653bf7842dd55cfea
Omar Rendon <omarcr.96@gmail.com>
2023-03-17 00:47:07 -0600

    Create a new directory to menu items

---
+++ src/menuItems/index.ts
@@ -0,0 +1,19 @@
+import {MenuItem} from '../interfaces/appInterfaces';
+
+export const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101',
+  },
+  {
+    name: 'Animatioin 102',
+    icon: 'albums-outline',
+    component: 'Animation102',
+  },
+  {
+    name: 'Switch',
+    icon: 'albums-outline',
+    component: 'SwitchScreen',
+  },
+];
