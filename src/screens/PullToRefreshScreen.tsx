4fc15f0769220335ff4829eaa1ca9b71c536549b
Omar Rendon <omarcr.96@gmail.com>
2023-03-24 14:57:03 -0600

    ADD: PullToRefresh screen

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, RefreshControl, ScrollView} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {HeaderTitle} from '../components/HeaderTitle';
 
 export const PullToRefreshScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      console.log('loading finished');
+      setRefreshing(false);
+    }, 1500);
+  };
+
   return (
-    <View>
-      <HeaderTitle title="Pull To Refresh" />
-    </View>
+    <ScrollView
+      style={{
+        marginTop: refreshing ? top : 0,
+      }}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      <View>
+        <HeaderTitle title="Pull To Refresh" />
+      </View>
+    </ScrollView>
   );
 };
