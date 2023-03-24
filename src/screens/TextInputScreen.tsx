367d1f85395c77b0d6300b57294b919ea0fe2fe2
Omar Rendon <omarcr.96@gmail.com>
2023-03-24 13:49:54 -0600

    CREATE: useForm hook and implement in TextinPUT SCREEN

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -6,24 +6,21 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  Text,
 } from 'react-native';
+import {CustomSwitch} from '../components/CustomSwitch';
 import {HeaderTitle} from '../components/HeaderTitle';
+import {useForm} from '../hooks/useForm';
 import {themeStyles} from '../theme/appTheme';
 
 export const TextInputScreen = () => {
-  const [formValues, setFormValues] = useState({
+  const {onChange, formValues} = useForm({
     name: '',
     lastName: '',
     email: '',
     phone: '',
+    isSuscribed: false,
   });
-
-  const onChange = (value: string, field: string) => {
-    setFormValues({
-      ...formValues,
-      [field]: value,
-    });
-  };
 
   return (
     <KeyboardAvoidingView
@@ -53,6 +50,14 @@ export const TextInputScreen = () => {
             onChangeText={value => onChange(value, 'email')}
             keyboardType="email-address"
           />
+          <Text>Suscribirme</Text>
+          <View style={styles.switchRow}>
+            <Text style={styles.switchText}>isHappy</Text>
+            <CustomSwitch
+              isOn={formValues.isSuscribed}
+              onChange={value => onChange(value, 'isSuscribed')}
+            />
+          </View>
           <HeaderTitle title={JSON.stringify(formValues, null, 3)} />
           <TextInput
             style={styles.textInput}
@@ -76,5 +81,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     marginVertical: 10,
+  },
+  switchRow: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    margin: 10,
+  },
+  switchText: {
+    fontSize: 25,
   },
 });
