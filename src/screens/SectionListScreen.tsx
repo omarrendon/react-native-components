f8f29164f975c661a2ccaded0949510d0aa2d82f
Omar Rendon <omarcr.96@gmail.com>
2023-03-24 16:49:38 -0600

    ADD: SectionListScreen info

--- src/screens/SectionListScreen.tsx
+++ src/screens/SectionListScreen.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
-import {View} from 'react-native';
+import {SectionList, Text, View} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
+import {themeStyles} from '../theme/appTheme';
+
+interface Casas {
+  casa: string;
+  data: string[];
+}
+
+const casas: Casas[] = [
+  {
+    casa: 'DC Comics',
+    data: ['Batman', 'Superman', 'Robin'],
+  },
+  {
+    casa: 'Marvel Comics',
+    data: ['Antman', 'Thor', 'Spiderman', 'Antman'],
+  },
+  {
+    casa: 'Anime',
+    data: ['Kenshin', 'Goku', 'Saitama'],
+  },
+];
 
 export const SectionListScreen = () => {
   return (
-    <View>
-      <HeaderTitle title="Section List" />
+    <View style={{...themeStyles.globalMargin, flex: 1}}>
+      <SectionList
+        ListHeaderComponent={() => <HeaderTitle title="Section List" />}
+        ListFooterComponent={() => (
+          <HeaderTitle title={'Total de secciones: ' + casas.length} />
+        )}
+        sections={casas}
+        renderItem={({item}) => <Text>{item}</Text>}
+        keyExtractor={(item, index) => item + index}
+        stickySectionHeadersEnabled
+        renderSectionHeader={({section}) => (
+          <HeaderTitle title={section.casa} />
+        )}
+      />
     </View>
   );
 };
